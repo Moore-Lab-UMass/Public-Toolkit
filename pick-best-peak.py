403913e331ab98912de341595fc560484ab9c774
Jill Moore <Jill.Moore@umassmed.edu>
2022-11-21 16:12:37 -0500

    fixed formatting

--- pick-best-peak.py
+++ pick-best-peak.py
@@ -4,11 +4,11 @@
 #November 2022
 
 import sys
-merge=open(sys.argv[1])
+merge = open(sys.argv[1])
 for line in merge:
-    line=line.rstrip().split("\t")
-    signal=[float(i) for i in line[4].split(",")]
-    peaks=line[3].split(",")
-    print (peaks[signal.index(max(signal))])
+    line = line.rstrip().split("\t")
+    signal = [float(i) for i in line[4].split(",")]
+    peaks = line[3].split(",")
+    print(peaks[signal.index(max(signal))])
 merge.close() 
     
